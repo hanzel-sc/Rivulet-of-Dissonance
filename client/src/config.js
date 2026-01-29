e2227eff26e5ef9c2363a397416ce80a66ac8dfb
hanzel_sc <hanselkansam04@gmail.com>
2026-01-29 14:46:33 +0530

    Commit#20: Fixed CORS issues with EC2

--- client/src/config.js
+++ client/src/config.js
@@ -3,4 +3,5 @@
 export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 // Helper function to build API URLs
-export const apiUrl = (path) => `${API_BASE_URL}${path}`;
+export const apiUrl = (path) =>
+    `${API_BASE_URL.replace(/\/$/, "")}/${path.replace(/^\//, "")}`;
